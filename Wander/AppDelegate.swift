dc786baeb4730e7a9b1440949decb68dc4bc313d
hpdnnr7 <hpdnnr7@gmail.com>
2021-04-21 02:00:43 -0400

    first phase

--- Wander/AppDelegate.swift
+++ Wander/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "KESC42AdY01nraMNg4YTYMM0jhepbvP6OYISPyc9" // <- UPDATE
+                    $0.clientKey = "TIVSjOA8Tg53cEh7jyjZsT01sdmimSQAkfli5j5i" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
